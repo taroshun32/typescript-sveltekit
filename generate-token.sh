b435d40da23660a9d0f025b44e77822a922c7237
taroshun32 <shunta.nakamura@nextbeat.net>
2023-07-13 11:19:06 +0900

    test

--- generate-token.sh
+++ generate-token.sh
@@ -8,22 +8,16 @@ sign() {
   openssl dgst -binary -sha256 -sign <(printf '%s' "${PRIVATE_KEY}")
 }
 
-echo "${PRIVATE_KEY}"
-
 header="$(printf '{"alg":"RS256","typ":"JWT"}' | base64url)"
-echo header=${header}
 
 now="$(date '+%s')"
 iat="$((now - 60))"
 exp="$((now + (3 * 60)))"
 template='{"iss":"%s","iat":%s,"exp":%s}'
 payload="$(printf "${template}" "${APP_ID}" "${iat}" "${exp}" | base64url)"
-echo payload=${payload}
 
 signature="$(printf '%s' "${header}.${payload}" | sign | base64url)"
-echo ${signature}
 jwt="${header}.${payload}.${signature}"
-echo ${jwt}
 
 installation_id="$(curl --location --silent --request GET \
   --url "https://api.github.com/app/installations" \
@@ -31,8 +25,6 @@ installation_id="$(curl --location --silent --request GET \
   --header "Authorization: Bearer ${jwt}" \
   | jq -r '.[0] | .id'
 )"
-
-echo ${installation_id}
 
 token="$(curl --location --silent --request POST \
   --url "https://api.github.com/app/installations/${installation_id}/access_tokens" \
